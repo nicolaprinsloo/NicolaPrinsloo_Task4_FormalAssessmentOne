b02eff49cc22ba4d1c635c7fe7e3fb468fa9600c
nicolaprinsloo <231021@virtualwindow.co.za>
2023-07-31 17:12:41 +0200

    Week2 Work Enter

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello");
+
+    // -----------------------------------------
+    // Home Page
+
+    // When the document loads, animate the hero image upwards
+    $(".hero-image").animate({top: '-=100px'});
+
+
+    //Tap on browse button
+    $("#btn2").click(function() {
+      $(".hero2-image").slideDown(2000).slideDown(2000);
+    });
+
+
+    // -----------------------------------------
+    // Browse Page
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+
+    }); 
+
+  // When the card is clicked
+  $(".card").click(function(){
+
+    // Toggle the price & description text
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit the additional content
+    $(".card-img-top").toggleClass("small");
+
+  });
