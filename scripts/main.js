3a750bf465006560321b2e045557b1c48c781c75
nicolaprinsloo <231021@virtualwindow.co.za>
2023-08-02 18:15:10 +0200

    fixed js

--- scripts/main.js
+++ scripts/main.js
@@ -2,39 +2,38 @@
 // Plant Array
 // -----------------------------------------
 
-const arrPlants = [
+const plants = [
   {
-    name: "Ficus Tree",
-    price: 350,
-    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior décor."
-    image: "plant1.png"
+    "name": "Ficus Tree",
+    "price": 350,
+    "description": "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
+    "image": "plant1.png"
   },
   {
-    name: "White Sprite Succulent",
-    price: 200,
-    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior décor."
-    image: "plant2.png"
+    "name": "White Sprite Succulent",
+    "price": 200,
+    "description": "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
+    "image": "plant2.png"
   },
   {
-    name: "Snake Plant",
-    price: 400,
-    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior décor."
-    image: "plant3.png"
+    "name": "Snake Plant",
+    "price": 400,
+    "description": "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
+    "image": "plant3.png"
   },
   {
-    name: "Parlour Palm",
-    price: 350,
-    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior décor."
-    image: "plant4.png"
+    "name": "Parlour Palm",
+    "price": 350,
+    "description": "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
+    "image": "plant4.png"
   },
   {
-    name: "Japanese Maple",
-    price: 1200,
-    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior décor."
-    image: "plant5.png"
+    "name": "Japanese Maple",
+    "price": 1200,
+    "description": "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
+    "image": "plant5.png"
   }
 ];
-
 
 
 
@@ -45,56 +44,30 @@ const arrPlants = [
 
 $(document).ready(function(){
 
-    console.log("Hello");
+  console.log("Hello");
 
-    // -----------------------------------------
-    // Home Page
+  // -----------------------------------------
+  // Home Page
 
-    // When the document loads, animate the hero image upwards
-    $(".hero-image").animate({top: '-=100px'});
+  // When the document loads, animate the hero image upwards
+  $(".hero-image").animate({top: '-=100px'});
 
+  // -----------------------------------------
+  // Browse Page
 
-    // -----------------------------------------
-    // Browse Page
-
-    // Hide all description text from the plant cards
-    $("#descriptionText").hide();
-    loadPlants();
+  loadPlants();
 
 }); 
 
-
-
-
-
-
-    // -----------------------------------------
-    // Pages
-    // -----------------------------------------
-
-    // -----------------------------------------
-    // Home Page
-
-    //Tap on browse button
-    $("#btn2").click(function() {
-      $(".hero2-image").slideDown(2000).slideDown(2000);
-    });
-
-
-
-    // -----------------------------------------
-    // Browse Page
-
-    // -----------------------------------------
-    // Load All Plants
-    // -----------------------------------------
+  // -----------------------------------------
+  // Load All Plants
+  // -----------------------------------------
 
     function loadPlants(){
 
-      console.log(arrPlants);
-
-      for (let i = 0; 0 < arrPlants.length; i++) {
-        const plant = arrPlants[i];
+      // Load and display all the plants
+      for (let i = 0; 0 < plants.length; i++) {
+        const plant = plants[i];
 
         console.log(plant);
         
@@ -104,30 +77,29 @@ $(document).ready(function(){
         //2: Create a variable that contains the most recently added plant card
         let currentChild = $("#plantContainer").children().eq(i+1);
 
-        //3: Set the content for the current plant card from the array
+        // 3: Set the content for the plant card from the plants list
+        $(currentChild).find(".card-img-top").attr('src','assets/' + plant.image);
         $(currentChild).find("#nameText").text(plant.name);
-        $(currentChild).find("#priceText").text(plant.price);
+        $(currentChild).find("#priceText").text('R' + plant.price);
         $(currentChild).find("#descriptionText").text(plant.description);
-        $(currentChild).find(".card-img-top").attr('src', 'assets/' + plant.image)
 
         //4: Hide the description text from the current card item
         $(currentChild).find("#descriptionText").hide();
 
       }
-
     };
 
-    // -----------------------------------------
-    // When the card is clicked
-    // -----------------------------------------
+  // -----------------------------------------
+  // When the card is clicked
+  // -----------------------------------------
 
     $("#plantsContainer").on('click','.card', function() {
   
 
       // Toggle the price & description text
-      $(this).find("#descriptionText").toggle();
       $(this).find("#priceText").toggle();
-
+      $(this).find("#descriptionText").toggle();
+      
       // Resize the image to fit the additional content
       $(this).find(".card-img-top").toggleClass("small");
 
