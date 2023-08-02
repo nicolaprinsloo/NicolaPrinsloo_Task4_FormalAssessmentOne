750e3e2b81f1c9afa46fb7819a9e06924a807878
nicolaprinsloo <231021@virtualwindow.co.za>
2023-08-02 17:35:57 +0200

    for loop and spacing

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 // Plant Array
 // -----------------------------------------
 
-let arrPlants = [
+const arrPlants = [
   {
     name: "Ficus Tree",
     price: 350,
@@ -36,6 +36,9 @@ let arrPlants = [
 ];
 
 
+
+
+
 // -----------------------------------------
 // When the document loads
 // -----------------------------------------
@@ -44,7 +47,6 @@ $(document).ready(function(){
 
     console.log("Hello");
 
-
     // -----------------------------------------
     // Home Page
 
@@ -52,19 +54,68 @@ $(document).ready(function(){
     $(".hero-image").animate({top: '-=100px'});
 
 
+    // -----------------------------------------
+    // Browse Page
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+    loadPlants();
+
+}); 
+
+
+
+
+
+
+    // -----------------------------------------
+    // Pages
+    // -----------------------------------------
+
+    // -----------------------------------------
+    // Home Page
+
     //Tap on browse button
     $("#btn2").click(function() {
       $(".hero2-image").slideDown(2000).slideDown(2000);
     });
 
 
+
     // -----------------------------------------
     // Browse Page
 
-    // Hide all description text from the plant cards
-    $("#descriptionText").hide();
+    // -----------------------------------------
+    // Load All Plants
+    // -----------------------------------------
 
-    }); 
+    function loadPlants(){
+
+      console.log(arrPlants);
+
+      for (let i = 0; 0 < arrPlants.length; i++) {
+        const plant = arrPlants[i];
+
+        console.log(plant);
+        
+        //1: Select the plants container and add the current array to it
+        $("#plantsContainer").append($("#plantCardTemplate").html());
+
+        //2: Create a variable that contains the most recently added plant card
+        let currentChild = $("#plantContainer").children().eq(i+1);
+
+        //3: Set the content for the current plant card from the array
+        $(currentChild).find("#nameText").text(plant.name);
+        $(currentChild).find("#priceText").text(plant.price);
+        $(currentChild).find("#descriptionText").text(plant.description);
+        $(currentChild).find(".card-img-top").attr('src', 'assets/' + plant.image)
+
+        //4: Hide the description text from the current card item
+        $(currentChild).find("#descriptionText").hide();
+
+      }
+
+    };
 
     // -----------------------------------------
     // When the card is clicked
@@ -81,4 +132,5 @@ $(document).ready(function(){
 
     });
 
+    
  
