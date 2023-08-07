3538e06a0acca1a36007d51567573bc06949e5f6
nicolaprinsloo <231021@virtualwindow.co.za>
2023-08-07 17:45:49 +0200

    Week 5 Class work

--- scripts/main.js
+++ scripts/main.js
@@ -2,38 +2,52 @@
 // Plants Array
 // ----------------------------------------------------------------
 
-const plants = [
+const arrPlants = [
   {
-    "name": "Fikus Tree",
+    "name": "Ficus Tree",
     "price": 350,
     "description": "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
-    "image": "plant1.png"
+    "image": "plant1.png",
+    "lightAmount": "low",
+    "addedDate": "2023-05-15"
+
   },
   {
     "name": "White Sprite Succulent",
     "price": 200,
     "description": "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
-    "image": "plant2.png"
+    "image": "plant2.png",
+    "lightAmount": "bright",
+    "addedDate": "2023-03-25"
   },
   {
     "name": "Snake Plant",
     "price": 400,
     "description": "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
-    "image": "plant3.png"
+    "image": "plant3.png",
+    "lightAmount": "low",
+    "addedDate": "2023-04-15"
   },
   {
     "name": "Parlour Palm",
     "price": 350,
     "description": "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
-    "image": "plant4.png"
+    "image": "plant4.png",
+    "lightAmount": "low",
+    "addedDate": "2023-03-16"
   },
   {
     "name": "Japanese Maple",
     "price": 1200,
     "description": "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
-    "image": "plant5.png"
+    "image": "plant5.png",
+    "lightAmount": "bright",
+    "addedDate": "2023-06-15"
   }
 ];
+
+let appliedFilter = "";
+let appliedSort = "date added";
 
 // ----------------------------------------------------------------
 // When the document loads
@@ -52,7 +66,7 @@ $(document).ready(function(){
     // -----------------------------------------
     // Browse Page
 
-    loadPlants();
+    loadPlants(arrPlants);
 
 }); 
 
@@ -60,18 +74,26 @@ $(document).ready(function(){
 // Load all plants
 // ----------------------------------------------------------------
 
-function loadPlants() {
+function loadPlants(plantsToShow) {
 
     // Load and display all the plants
-    for (let i = 0; i < plants.length; i++) {
-      const plant = plants[i];
+
+    console.log(plantsToShow);
+
+    // Clear all elements in container
+    $("#plantsContainer").empty();
+
+    // Loop through plants
+
+    for (let i = 0; i < plantsToShow.length; i++) {
+      const plant = plantsToShow[i];
       
       console.log(plant);
 
       // 1: Select plants container and add the current array plant to it
       $("#plantsContainer").append($("#plantCardTemplate").html());
 
-      // 2: Create a varible that contains the most recently added plant card
+      // 2: Create a variable that contains the most recently added plant card
       let currentChild = $("#plantsContainer").children().eq(i+1);
 
       // 3: Set the content for the plant card from the plants list
@@ -85,6 +107,38 @@ function loadPlants() {
 
     }
 }
+
+// ----------------------------------------------------------------
+// When a filter or sort is clicked
+// ----------------------------------------------------------------
+
+$("input[name='filterRadio']").click(Function() {
+  appliedFilter = $(this).attr('value');
+
+  console.log(appliedFilter);
+  filterSortPlants();
+});
+
+$("input[name='sortRadio']").click(Function() {
+  appliedSort = $(this).attr('value');
+
+  console.log(appliedSort);
+  filterSortPlants();
+});
+
+function filterSortPlants() {
+
+  let filteredSortedArrPlants = [];
+
+  //Filter Plants
+  filteredSortedArrPlants = arrPlants.filter(plant => plant.lightAmount == appliedFilter)
+  
+  loadPlants(filteredSortedArrPlants);
+
+  
+
+}
+
 
 // ----------------------------------------------------------------
 // When the plant card is clicked
